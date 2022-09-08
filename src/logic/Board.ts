640eeb46f262f62142578acf6de4f854611f90b6
Radomir Wojtera <radomir.wojtera@gmail.com>
2022-09-08 02:56:51 +0200

    First steps with the board. 3A.M. Time to sleep.

--- src/logic/Board.ts
+++ src/logic/Board.ts
@@ -1,3 +1,4 @@
+import Direction from "./Direction";
 import Vector3 from "./Vector3";
 
 // coordinates
@@ -8,19 +9,75 @@ import Vector3 from "./Vector3";
 // / x \
 // \y_z/
 
-interface IList<T> {
-  [index: number]: T;
+class CellPosition extends Vector3<number> {}
+
+class Cell {
+  position: CellPosition;
+  value: number = 0;
+
+  constructor(x: number, y: number, z: number) {
+    this.position = new CellPosition(x, y, z);
+  }
+
+  get x() {
+    return this.position.x;
+  }
+  get y() {
+    return this.position.y;
+  }
+  get z() {
+    return this.position.z;
+  }
 }
 
-class Direction extends Vector3<-1 | 0 | 1> {
-  static readonly UP = new Direction(0, 1, -1);
-  static readonly DOWN = new Direction(0, -1, 1);
-  static readonly LEFT_UP = new Direction(-1, 1, 0);
-  static readonly RIGHT_DOWN = new Direction(1, -1, 0);
-  static readonly RIGHT_UP = new Direction(1, 0, -1);
-  static readonly LEFT_DOWN = new Direction(-1, 0, 1);
-}
+export default class Board {
+  radius: number;
+  cells: Cell[];
+  cube: Map<number, Map<number, Map<number, Cell>>> = new Map();
 
-class Board {
-  radius: number = 0;
+  private howManyCells: number;
+  private maxCellsInLine: number;
+
+  constructor(radiusIncludingCenter: number = 2) {
+    this.radius = radiusIncludingCenter - 1;
+    this.howManyCells = 1 + 3 * this.radius * (this.radius + 1);
+    this.maxCellsInLine = 2 * this.radius + 1;
+    this.cells = new Array(this.howManyCells);
+
+    this.buildCells(); // could be done lazy, but there's no real need for this for this task
+
+    this.findLanes();
+  }
+
+  buildCells(): void {
+    let cellsCount = 0;
+    for (let x = -this.radius; x <= this.radius; x++) {
+      const minY = Math.max(-this.radius, -x - this.radius);
+      const maxY = Math.min(this.radius, -x + this.radius);
+
+      for (let y = minY; y <= maxY; y++) {
+        this.addCell(new Cell(x, y, -x - y), cellsCount++);
+      }
+    }
+  }
+
+  addCell(cell: Cell, position: number): void {
+    this.cells[position] = cell;
+
+    if (!this.cube.has(cell.x)) {
+      this.cube.set(cell.x, new Map<number, Map<number, Cell>>());
+    }
+
+    const xLayer = this.cube.get(cell.x);
+
+    if (!xLayer?.has(cell.y)) {
+      xLayer?.set(cell.y, new Map<number, Cell>());
+    }
+
+    const yLayer = xLayer?.get(cell.y);
+
+    yLayer?.set(cell.z, cell);
+  }
+
+  findLanes(): void {}
 }
--- src/logic/Vector3.ts
+++ src/logic/Vector3.ts
@@ -1,18 +1,34 @@
 export default class Vector3<T> implements Iterable<T> {
-  readonly x: T;
-  readonly y: T;
-  readonly z: T;
+  protected _x: T;
+  protected _y: T;
+  protected _z: T;
 
-  [index: number]: T;
+  readonly [index: number]: T;
 
   constructor(x: T, y: T, z: T) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
+    this._x = x;
+    this._y = y;
+    this._z = z;
+  }
 
-    this[0] = this.x;
-    this[1] = this.y;
-    this[2] = this.z;
+  get [0]() {
+    return this._x;
+  }
+  get [1]() {
+    return this._y;
+  }
+  get [2]() {
+    return this._z;
+  }
+
+  get x() {
+    return this._x;
+  }
+  get y() {
+    return this._y;
+  }
+  get z() {
+    return this._z;
   }
 
   [Symbol.iterator](): Iterator<T> {
