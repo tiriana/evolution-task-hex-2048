7d22393aed3c02ad3f81d4873d1600440a3ebdb1
Radomir Wojtera <radomir.wojtera@gmail.com>
2022-09-08 18:41:26 +0200

    Somethings getting rendered, but we're far from ideal

--- src/components/App/SimpleBoard.tsx
+++ src/components/App/SimpleBoard.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import { Board as BoardLogic, Cell } from "../../logic/GameLogic";
 import SimpleCell from "./SimpleCell";
 
+import {
+  HexGrid,
+  Layout,
+  Hexagon,
+  Text,
+  Pattern,
+  Path,
+  Hex,
+} from "react-hexgrid";
+
 type SimpleBoardProps = {
   board: BoardLogic;
 };
@@ -10,11 +20,18 @@ const SimpleBoard: React.FC<SimpleBoardProps> = ({
   board,
 }: SimpleBoardProps) => {
   return (
-    <div>
-      {board.cells.map((cell: Cell) => {
-        return <SimpleCell cell={cell} />;
-      })}
-    </div>
+    <HexGrid width={1200} height={800} viewBox="-50 -50 100 100">
+      <Layout
+        size={{ x: 10, y: 10 }}
+        flat={true}
+        spacing={1.1}
+        origin={{ x: 0, y: 0 }}
+      >
+        {board.cells.map((cell: Cell) => {
+          return <SimpleCell key={cell.position.toString()} cell={cell} />;
+        })}
+      </Layout>
+    </HexGrid>
   );
 };
 
--- src/components/App/SimpleCell.tsx
+++ src/components/App/SimpleCell.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { Cell } from "../../logic/GameLogic";
 
+import { Hexagon, Text, Pattern, Path, Hex } from "react-hexgrid";
+
+import style from "./SimpleCell.module.scss";
+
 type SimpleCellProps = {
   cell: Cell;
 };
 
 const SimpleCell: React.FC<SimpleCellProps> = ({ cell }: SimpleCellProps) => {
   return (
-    <div>
-      [{cell.x}, {cell.y}, {cell.z}]: {cell.value}
-    </div>
+    // <div>
+    //   [{cell.x}, {cell.y}, {cell.z}]: {cell.value}
+    // </div>
+    <Hexagon
+      className={[style.cell, style[`tile${cell.value}`]].join(" ")}
+      data={cell.position}
+      q={cell.x}
+      r={cell.y}
+      s={cell.z}
+      fill="black"
+    >
+      <Text className={style.text}>{cell.value}</Text>
+    </Hexagon>
   );
 };
 
