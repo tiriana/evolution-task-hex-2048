2a120e407b779dd2be781d58d85317eaa060e0bf
Radomir Wojtera <radomir.wojtera@gmail.com>
2022-09-09 18:59:40 +0200

    Sets the right headers

--- src/components/App/GetData.tsx
+++ src/components/App/GetData.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { GameConfig } from "./GameConfig";
 import { RngServerResponse } from "./RngServerResponse";
 import { RngServerRequest } from "./RngServerRequest";
-import axios, { AxiosError, AxiosResponse } from "axios";
 
 type GetDataProps = GameConfig & {
   onReceived: (r: RngServerResponse) => void;
@@ -16,15 +15,32 @@ export const GetData: React.FC<GetDataProps> = ({
   request,
 }) => {
   useEffect(() => {
-    const url = `${window.location.protocol}://${hostname}:${port}/${radius}`;
+    const url = `//${hostname}:${port}/${radius}`;
 
-    axios
-      .post(url, request)
-      .then((response: AxiosResponse<RngServerResponse>) => {
-        onReceived(response.data);
-      })
-      .catch((error: AxiosError) => {
-        console.error(error); // TODO add better error handling
+    fetch(url, {
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        pragma: "no-cache",
+        "sec-ch-ua":
+          '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Linux"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+      },
+      referrerPolicy: "no-referrer",
+      body: "[]",
+      method: "POST",
+      mode: "cors",
+      credentials: "omit",
+    })
+      .then((r) => r.json())
+      .then((response: RngServerResponse) => {
+        onReceived(response);
       });
   });
 
